b93c177a459d1ef74c394fc5014957c4a170fafc
Oliver Kurmis <oliver@kurmis.com>
2015-08-16 22:30:58 +0200

    improve ruby code

--- primes.rb
+++ primes.rb
@@ -1,8 +1,18 @@
 #!/usr/bin/ruby
 
-n = Integer ARGV[0]
+n = ARGV[0].to_i
 nums = [nil, nil, *2..n]
-(2..Math.sqrt(n)).each do |i|
-    (i**2..n).step(i){|m| nums[m] = nil} if nums[i]
+
+upper_lim = (Math.sqrt n.to_f).to_i
+i = 2
+while i <= upper_lim
+    if nums[i]
+		m=i**2
+		while m <= n 
+			nums[m] = nil
+			m+=i
+		end
+	end
+    i += 1
 end
 p nums.compact.size
