1ae6544997b37df6f1b479a1a1a8dccaa1478093
Oliver Kurmis <oliver@kurmis.com>
2015-08-18 00:16:36 +0200

    fix php code, improve counting of primes

--- primes.php
+++ primes.php
@@ -2,16 +2,11 @@
 if (empty($argv[1])) die('missing parameter limit');
 $limit = (int) $argv[1];
 
-$composite=[];
+$nums=[];
 $sqrtlimit = (int)sqrt($limit);
 for ($n = 2; $n <= $sqrtlimit; $n++)
-	if (empty($composite[$n]))
-		for ($i = $n*$n; $i < $limit; $i += $n)
-			$composite[$i] = true;
- 
-$numprimes=0;
-for ($n = 2; $n < $limit; $n++)
-	if (empty($composite[$n]))
-		$numprimes++;
+	if (!isset($nums[$n]))
+		for ($i = $n*$n; $i <= $limit; $i += $n)
+			$nums[$i] = 1;
 
-echo $numprimes."\n";
+echo $limit-1-array_sum($nums)."\n";
